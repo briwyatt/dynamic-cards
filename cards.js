44a61b4e105cd58c6eac5783de247843c15eb663
Bri Wyatt <mbriwyatt@gmail.com>
2017-02-02 17:46:58 -0600

    inital commit add input text nodeto card div

---
+++ cards.js
@@ -0,0 +1,79 @@
+console.log("hello dynamic cards!");
+
+
+
+
+function createCard() {
+    var oneCard = document.getElementById("new-card-element");
+    oneCard.parentNode.appendChild();
+}
+
+
+function removeFromDom() {
+    var oneCard = document.getElementById("new-card-element");
+    oneCard.parentNode.removeChild();
+}
+
+
+
+var userInput = document.getElementById("textarea");
+console.log("userInput",userInput);
+console.log("userInput.value",userInput.value);
+var userSubmit = document.getElementById("create-button")
+var createCardUponClick = userSubmit.addEventListener("click", createCard);
+
+// Create a <p> element and append it to a <div> element:
+
+    var collectedInput = userInput.value;					
+    // console.log("collectedInput", collectedInput);			// userInput.value = outputs the actual text the user submitted
+
+	var newCardBody = document.createElement("Div");			// create a <div> element node for card's body = var newCardBody
+	newCardBody.id = "newCard";									// give newCardBody an id="newCard"
+    var para = document.createElement("P"); 					// Create a <p> element node = var para
+    var deleteButton = document.createElement("Button");		// create <button> element node = var deleteButton
+    // deleteButton.id = "deleteButton";						// add id to button				
+    deleteButton.innerHTML = "delete me"; 					    // add text to delete button
+	
+
+
+// 	var para = document.createElement("P");                       // Create a <p> element
+// var t = document.createTextNode("This is a paragraph.");      // Create a text node
+// para.appendChild(t);   
+
+console.log("userInput", userInput);
+
+	var text = document.createTextNode(collectedInput);		    // Create a text node element (reference to the user's input)
+
+
+function createCard() {
+    // console.log("newCardBody", newCardBody);
+
+  	cardWrapper.appendChild(newCardBody);  						 	// Append new card body/ outline 
+   		 newCardBody.appendChild(para); 					// Append <p> to <div> with id="newCardBody"
+   		 console.log("para",para);
+    			para.appendChild(text); 						// append <text node> with user's submitted input text 	to <p>
+    			console.log("text",text);
+   		 newCardBody.appendChild(deleteButton);
+};
+
+// function deleteCard(){
+
+// }
+
+//"removeChild(child)" REFERENCE SYNTAX FROM MOZILLA:
+// The Node.removeChild() method removes a child node from the DOM. Returns removed node.
+// var oldChild = node.removeChild(child);
+// OR
+// element.removeChild(child);
+
+
+// child is the child node to be removed from the DOM.
+// node is the parent node of child.
+// oldChild holds a reference to the removed child node. oldChild === child.
+
+
+// *** ME TRYING TO SET THE REFERENCE SYNTAX ABOVE TO MY PROJECT: ***
+
+// var oldChild = card-wrapper.removeChild(card-element);
+// OR
+// element.removeChild(child);
