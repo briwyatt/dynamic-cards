3c3df16a45ebd16ead3693748b9f0e377b49e034
Bri Wyatt <mbriwyatt@gmail.com>
2017-02-05 20:20:17 -0600

    fixing delete button functionality

--- cards.js
+++ cards.js
@@ -1,69 +1,40 @@
-console.log("hello dynamic cards!");
+console.log("hello dynamic cards JS!");
 
+var cardWrapper;
+document.getElementById("create-button").addEventListener("click", createCard);
+var deleteButton;
+var newCardBody;
 
-
-
-function createCard() {
-    var oneCard = document.getElementById("new-card-element");
-    oneCard.parentNode.appendChild();
-}
-
-
-// function removeFromDom() {
-//     var oneCard = document.getElementById("new-card-element");
-//     oneCard.parentNode.removeChild();
-// }
-
-
-
-var userInput = document.getElementById("textarea");
-var userSubmit = document.getElementById("create-button")
-var createCardUponClick = userSubmit.addEventListener("click", createCard);
-
-// Create a <p> element and append it to a <div> element:
-
-
-var newCardBody = document.createElement("Div"); // create a <div> element node for card's body = var newCardBody
-newCardBody.id = "newCard"; // give newCardBody an id="newCard"
-var para = document.createElement("P"); // Create a <p> element node = var para
-var deleteButton = document.createElement("Button"); // create <button> element node = var deleteButton
-// deleteButton.id = "deleteButton";						// add id to button				
-deleteButton.innerHTML = "delete me"; // add text to delete button
-
-
-
-
-
-function createCard() {
-    // console.log("newCardBody", newCardBody);
-
-    cardWrapper.appendChild(newCardBody); // Append new card body/ outline 
-    newCardBody.appendChild(para);    // Append <p> to <div> with id="newCardBody"
-    // console.log("para", para);
-    // console.log("userInput.value", userInput.value);
+function createCard(e) {
+    // console.log("userInput.value",userInput.value);
+    var userInput = document.getElementById("textarea");
     var text = document.createTextNode(userInput.value); // Create a text node element (reference to the user's input)
+    console.log("e.target", e.target);
+    console.log("userInput", userInput);
+
+    cardWrapper = document.getElementById("cardWrapper")
+    newCardBody = document.createElement("Div"); // create a <div> element node for card's body = var newCardBody
+    newCardBody.id = "card-wrap";
+    console.log("newCardBody", newCardBody); // outputs the new html elements/nodes for the card includes: div#newCard>p>userInput.valuetextnode^+deleteBtn
+    cardWrapper.appendChild(newCardBody); // Append new card body/ outline 
+
+    var para = document.createElement("P"); // Create a <p> element node = var para
+    newCardBody.appendChild(para); // Append <p> element to the <div> element:
+    // console.log("userInput.value", userInput.value);
     para.appendChild(text); // append <text node> with user's submitted input text   to <p>
-    newCardBody.appendChild(deleteButton);
+
+    var deleteButton = document.createElement("Button"); // create <button> element node = var deleteButton
+    newCardBody.appendChild(deleteButton); //append delete button to the card
+    deleteButton.innerHTML = "delete me"; // add text to delete button
+    deleteButton.className = "deleteButtonClass";
+
+    console.log("deleteButton", deleteButton)
+    deleteButton.addEventListener("click", deleteCard);
 };
 
-// function deleteCard(){
-
-// }
-
-//"removeChild(child)" REFERENCE SYNTAX FROM MOZILLA:
-// The Node.removeChild() method removes a child node from the DOM. Returns removed node.
-// var oldChild = node.removeChild(child);
-// OR
-// element.removeChild(child);
-
-
-// child is the child node to be removed from the DOM.
-// node is the parent node of child.
-// oldChild holds a reference to the removed child node. oldChild === child.
-
-
-// *** ME TRYING TO SET THE REFERENCE SYNTAX ABOVE TO MY PROJECT: ***
-
-// var oldChild = card-wrapper.removeChild(card-element);
-// OR
-// element.removeChild(child);
+function deleteCard(e) {
+    var node = document.getElementById("card-wrap");
+    if (node.parentNode) {
+        node.parentNode.removeChild(node);
+    };
+}
